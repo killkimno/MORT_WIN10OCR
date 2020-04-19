3543d1d0a864abe453c13721f3c471a58543e998
killkimno <killkimno@hotmail.com>
2020-04-19 18:27:53 +0900

    upload

--- MORT_WIN10OCR/MORT_WIN10OCR/Class1.cs
+++ MORT_WIN10OCR/MORT_WIN10OCR/Class1.cs
@@ -38,6 +38,7 @@ namespace MORT_WIN10OCR
         private ProcessType processType = ProcessType.Ready;
         public static Class1 instance = new Class1();
         public OcrEngine ocrEngine = null;
+        public OcrResult ocrResult = null;
 
         //OCR 에 사용할 이미지 저장.
         public static string TestOpenCv(List<int> r, List<int> g, List<int> b, int x, int y)
@@ -168,27 +169,16 @@ namespace MORT_WIN10OCR
             //resultString = "sdfsdf@!ER";
             if (processType != ProcessType.Process)
             {
-                //await LoadSampleImage();
-                /*
-                OcrEngine ocrEngine = null;
-                if (code == "")
-                {
-                    ocrEngine = OcrEngine.TryCreateFromUserProfileLanguages();
-                }
-                else
-                {
-                    Language ocrLanguage = new Language(code);
-                    ocrEngine = OcrEngine.TryCreateFromLanguage(ocrLanguage);
-                }
-                */
-
+                
                 if (bitmap2 != null && ocrEngine != null)
                 {
                     processType = ProcessType.Process;
                     //resultString = "1";
-                    OcrResult ocrResult = await ocrEngine.RecognizeAsync(bitmap2);
+                    ocrResult = null;
+                    ocrResult = await ocrEngine.RecognizeAsync(bitmap2);
                     resultString = ocrResult.Text;
                     processType = ProcessType.Ready;
+                    
                 }
                 else
                 {
@@ -199,9 +189,88 @@ namespace MORT_WIN10OCR
             else
             {
                 resultString = " not ready";
+            }                       
+        }
+
+        //라인 수, 문장 리스트, 각 라인 단어 수
+        public struct OcrResultData
+        {
+            public bool isEmpty;
+            public int lineCount;       //라인 수.
+            public string[] words;      //모든 문장.
+            public double[] x;             //x값들
+            public double[] y;             //y값들
+            public double[] sizeX;         //size x;
+            public double[] sizeY;         //size y;
+            public int[] wordCounts;    //각 라인마다 워드 수.
+
+            public int wordsIndex;
+        }
+
+        public static IntPtr TestMar()
+        {
+
+            OcrResultData data = new OcrResultData();
+            if(instance.ocrResult != null)
+            {
+                data.lineCount = instance.ocrResult.Lines.Count;
+                if(data.lineCount == 0)
+                {
+                    data.isEmpty = true;
+
+                    data.wordCounts = new int[1];
+                    data.words = new string[1];
+                    data.x = new double[1];
+                    data.y = new double[1];
+                    data.sizeY = new double[1];
+                    data.sizeX = new double[1];
+                }
+                else
+                {
+                    data.isEmpty = false;
+                    int totalWordCount = 0;
+                    data.wordCounts = new int[ data.lineCount];
+                    for(int i = 0; i < instance.ocrResult.Lines.Count; i++)
+                    {
+                        int wordCount = 0;
+                        for(int j = 0; j < instance.ocrResult.Lines[i].Words.Count; j++)
+                        {
+                            wordCount++;
+                            totalWordCount++;
+                        }
+                        data.wordCounts[i] = wordCount;
+                    }
+
+                    data.wordsIndex = totalWordCount;
+                    data.words = new string[totalWordCount];
+                    data.x = new double[totalWordCount];
+                    data.y = new double[totalWordCount];
+                    data.sizeY = new double[totalWordCount];
+                    data.sizeX = new double[totalWordCount];
+
+                    int index = 0;
+                    for (int i = 0; i < instance.ocrResult.Lines.Count; i++)
+                    {
+                        for (int j = 0; j < instance.ocrResult.Lines[i].Words.Count; j++)
+                        {
+                            data.words[index] = instance.ocrResult.Lines[i].Words[j].Text;
+                            data.x[index] = instance.ocrResult.Lines[i].Words[j].BoundingRect.X;
+                            data.y[index] = instance.ocrResult.Lines[i].Words[j].BoundingRect.Y;
+                            data.sizeY[index] = instance.ocrResult.Lines[i].Words[j].BoundingRect.Height;
+                            data.sizeX[index] = instance.ocrResult.Lines[i].Words[j].BoundingRect.Width;
+                            index++;
+                        }
+                                  
+                    }
+
+                }
             }
 
-           
+            IntPtr pnt = Marshal.AllocHGlobal(Marshal.SizeOf(data));
+            Marshal.StructureToPtr(data, pnt, false);
+
+
+            return pnt;
         }
 
         public static string GetText()
